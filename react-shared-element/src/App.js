3274f9eb9aca40090b27734f18a35af9f7314f90
Jack Yang <jackya@trutestdairy.co.nz>
2020-01-17 18:09:46 +1300

    test

---
+++ react-shared-element/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SharedElement from './components/SharedElement'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SharedElement>eeeerrr</SharedElement>
+      </div>
+    )
+  }
+}
+
+// module.exports = SharedElement
+export default App
