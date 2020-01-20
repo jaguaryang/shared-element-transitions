9ca18af086bd2270646fa0ffbed614f7f4af5c33
Jack Yang <jackya@trutestdairy.co.nz>
2020-01-20 14:59:31 +1300

    build

---
+++ react-shared-element-example/build/precache-manifest.9d8e3d0651d4238c23cc285d70541056.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f47655deb4b4ea44825f7182d60358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b40455aebf558601aed3",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a2e8b387ceb6ece14775",
+    "url": "/static/js/2.4bb3631c.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.4bb3631c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b40455aebf558601aed3",
+    "url": "/static/js/main.d3f746b6.chunk.js"
+  },
+  {
+    "revision": "8d3f806f763ae9e886e9d32a5e79f95b",
+    "url": "/static/js/main.d3f746b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11f89743e5bacc013d03",
+    "url": "/static/js/runtime-main.3bb98ae5.js"
+  }
+]);
