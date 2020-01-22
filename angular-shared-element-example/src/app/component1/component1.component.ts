74bca876a15c47cf6a109d324ba41e71aea007bf
Jack Yang <jackya@trutestdairy.co.nz>
2020-01-22 15:20:55 +1300

    angular router works

--- angular-shared-element-example/src/app/component1/component1.component.ts
+++ angular-shared-element-example/src/app/component1/component1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-component1',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Component1Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  public push(event) {
+    this.router.navigate(['/component2'])
+  }
+
 
 }
--- angular-shared-element-example/src/app/component2/component2.component.ts
+++ angular-shared-element-example/src/app/component2/component2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-component2',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Component2Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public push(event) {
+    this.router.navigate(['/component1'])
   }
 
 }
