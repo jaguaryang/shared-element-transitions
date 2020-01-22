8e6137ccde0bc4e7555235f09398ed6663a3d7cc
Jack Yang <jackya@trutestdairy.co.nz>
2020-01-22 15:09:18 +1300

    angular router

--- angular-shared-element-example/src/app/app.module.ts
+++ angular-shared-element-example/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SharedElementComponent } from './shared-element/shared-element.component';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+
+
+const appRoutes: Routes = [
+  { path: 'component1', component: Component1Component },
+  { path: 'component2', component: Component2Component },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedElementComponent
+    SharedElementComponent,
+    Component1Component,
+    Component2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
