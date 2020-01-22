b39e6df7422fb3a609fd70ac2d90d70e7bc09971
Jack Yang <jackya@trutestdairy.co.nz>
2020-01-22 17:16:03 +1300

    angular init

--- angular-shared-element-example/src/app/component1/component1.component.ts
+++ angular-shared-element-example/src/app/component1/component1.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router"
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from "@angular/router"
+import { SharedElementComponent } from './../shared-element/shared-element.component';
 
 @Component({
   selector: 'app-component1',
   templateUrl: './component1.component.html',
-  styleUrls: ['./component1.component.css']
+  styleUrls: ['./component1.component.css'],
 })
 export class Component1Component implements OnInit {
 
   constructor(private router: Router) { }
 
+  @ViewChild(SharedElementComponent, { static: false }) private SharedElement: SharedElementComponent;
+
   ngOnInit() {
   }
 
   public push(event) {
-    this.router.navigate(['/component2'])
+    this.SharedElement.redirect(() => {
+      this.router.navigate(['/component2'])
+    });
+
   }
 
 
--- angular-shared-element-example/src/app/shared-element/shared-element.component.ts
+++ angular-shared-element-example/src/app/shared-element/shared-element.component.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import html2canvas from 'html2canvas'
 
 @Component({
-  selector: 'app-shared-element',
+  selector: 'SharedElement',
   templateUrl: './shared-element.component.html',
   styleUrls: ['./shared-element.component.css']
 })
 export class SharedElementComponent implements OnInit {
 
+  @Input() id: string
+  @Input() duration: number = 300
+  @Input() zindex: number = 1
+  @Input() transitionStart: Function
+  @Input() transitionEnd: Function
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.transition()
+  }
+
+  redirect = (callback) => {
+    if (!this.id) return
+
+    let id_common = this.id + "-common"
+    let node = document.getElementById(id_common)
+    if (node) {
+      node.remove();
+    }
+
+    node = document.createElement("DIV");
+    node.setAttribute("style", "position: absolute; background-size: 100% 100%; transition: all " + (this.duration / 1000) + "s; z-index: " + this.zindex + ";");
+    node.id = id_common;
+    document.body.appendChild(node);
+
+    let current = document.getElementById(this.id)
+    html2canvas(current, { scale: 1 }).then(canvas => {
+      let rect = current.getBoundingClientRect()
+      node.style.top = rect.top + 'px'
+      node.style.left = rect.left + 'px'
+      node.style.width = canvas.width + 'px'
+      node.style.height = canvas.height + 'px'
+      node.style.background = 'url(' + canvas.toDataURL() + ')'
+      if (callback) callback()
+    })
+  }
+
+  transition = () => {
+    if (!this.id) return
+
+    let id_common = this.id + "-common"
+    let node = document.getElementById(id_common)
+    if (!node) {
+      this.visible(true)
+      return
+    }
+    this.visible(false)
+
+    if (this.transitionStart) {
+      this.transitionStart()
+    }
+
+    let current = document.getElementById(this.id)
+    html2canvas(current, { scale: 1 }).then(canvas => {
+      let rect = current.getBoundingClientRect()
+      node.style.top = rect.top + 'px'
+      node.style.left = rect.left + 'px'
+      node.style.width = canvas.width + 'px'
+      node.style.height = canvas.height + 'px'
+      node.style.background = 'url(' + canvas.toDataURL() + ')'
+      setTimeout(() => {
+        node.remove();
+        if (this.transitionEnd) {
+          this.transitionEnd()
+        }
+        this.visible(true)
+      }, this.duration);
+    })
+  }
+
+  visible = (bol) => {
+    if (!this.id) return
+
+    const el: HTMLElement = document.getElementById(this.id).parentElement
+    el.style.opacity = bol ? "1" : "0"
   }
 
 }
